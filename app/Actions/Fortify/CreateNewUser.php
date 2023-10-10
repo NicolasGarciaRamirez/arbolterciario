ea4832b16f1956206d5a4bebd33d24239be9a2cf
Nicolas Garcia Ramirez <49569633+NicolasGarciaRamirez@users.noreply.github.com>
2023-10-09 20:20:54 -0500

    Update CreateNewUser.php

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -25,31 +25,59 @@ class CreateNewUser implements CreatesNewUsers
         ])->validate();
 
 		return DB::transaction(function () use ($input) {
-            $sponsor = User::find($input['patrocinador_id']);
-
-            // Check if the sponsor has reached the maximum number of referrals at their current level
-            if ($this->hasReferralLimitReached($sponsor, $sponsor->level+1)) {
-                throw ValidationException::withMessages([
-                    'patrocinador_id' => ['The sponsor has reached the referral limit at this level.'],
-                ]);
-            }
-
-            $level = $sponsor->level + 1;
-
-            // Create the new user
-            $user = User::create([
-                'name' => $input['name'],
-                'email' => $input['email'],
-                'password' => Hash::make($input['password']),
-                'patrocinador_id' => $input['patrocinador_id'],
-                'level' => $level,
-            ]);
-
-            return $user;
-        });
+			$sponsor = User::find($input['patrocinador_id']);
+	
+			// Determinar el nivel inicial para buscar espacio disponible
+			$startingLevel = $sponsor->level + 1;
+			$level = $startingLevel;
+	
+			while (true) {
+				// Verificar si el sponsor ha alcanzado el límite en este nivel
+				if ($this->hasReferralLimitReached($sponsor, $level)) {
+					$level++;
+				} else {
+					// Encontrar un lugar disponible en este nivel
+					$referralsAtLevel = $this->getReferralsAtLevel($sponsor, $level);
+	
+					// Buscar el primer lugar disponible en el nivel
+					$availableReferral = $referralsAtLevel->first(function ($user) use ($level) {
+						return $user->referidos->where('level', $level)->count() < 3;
+					});
+	
+					if ($availableReferral) {
+						// Colocar al nuevo usuario en uno de los lugares disponibles
+						$user = User::create([
+							'name' => $input['name'],
+							'email' => $input['email'],
+							'password' => Hash::make($input['password']),
+							'patrocinador_id' => $input['patrocinador_id'],
+							'level' => $level,
+						]);
+	
+						return $user;
+					}
+				}
+	
+				// Si no se encuentra espacio disponible en este nivel, continuar con el siguiente nivel
+				$level++;
+				
+				// Si llegamos al nivel 10 (por ejemplo), puedes agregar una lógica para manejar esto según tus necesidades.
+				if ($level > 10) {
+					throw ValidationException::withMessages([
+						'patrocinador_id' => ['No hay espacio disponible en ningún nivel.'],
+					]);
+				}
+			}
+		});
     }
 
-    private function hasReferralLimitReached($user, $level)
+    private function getReferralsAtLevel($user, $level)
+	{
+		// Obtener todos los referidos en el mismo nivel que el usuario
+		return $user->referidos->where('level', $level);
+	}
+	
+	private function hasReferralLimitReached($user, $level)
     {
         // Count the number of referrals at the same level as the sponsor
         $referralCount = $user->referidos->where('level', $level)->count();
@@ -57,4 +85,3 @@ class CreateNewUser implements CreatesNewUsers
         // Check if the sponsor has reached the maximum of 3 referrals at this level
         return $referralCount >= 3;
     }
-}
